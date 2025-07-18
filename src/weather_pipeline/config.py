4ca473f884e9c15ecec5751661db509cca09b8fd
nannasci <nannayusuf@gmail.com>
2025-07-18 19:27:12 -0300

    fix: ensure WEATHER_API_KEY is always read from env

--- src/weather_pipeline/config.py
+++ src/weather_pipeline/config.py
@@ -1,8 +1,7 @@
+# src/weather_pipeline/config.py
 from functools import lru_cache
-
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -12,7 +11,6 @@ class Settings(BaseSettings):
     )
 
     weather_api_key: str = Field(..., validation_alias="WEATHER_API_KEY")
-
 
 @lru_cache
 def get_settings() -> Settings:
